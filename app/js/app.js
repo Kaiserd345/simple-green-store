40d700b28ec97ac01edaa2f50a40967522bebf7d
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-11 15:25:21 +0200

    add interaction links all over the web page

--- app/js/app.js
+++ app/js/app.js
@@ -38,4 +38,20 @@ $(document).ready(function() {
     }
 
     tabToggle();
+
+    //Smooth Scroll
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+
+            const blockID = anchor.getAttribute('href').substr(1)
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    }
 })
