a428d977c50a6e54420195722cd7e837103d9ab1
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-11 13:53:03 +0200

    add functionality for switching tabs at product section

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    window.onscroll = function () {stickyHeader()};
-    let headerTop = document.querySelector(".header");
-    let sticky = headerTop.offsetTop;
+    //Sticky Header
+    window.onscroll = () => stickyHeader();
+    const headerTop = document.querySelector(".header");
 
     function stickyHeader() {
         if (window.pageYOffset >= 150) {
@@ -10,4 +10,32 @@ $(document).ready(function() {
             headerTop.classList.remove("header-sticky")
         }
     }
+
+    //Tabs Toggle for Brand Section
+    const tabToggle = function () {
+        const tabNav = document.querySelectorAll('.products-content-categories-button');
+        const tabContent = document.querySelectorAll('.cat-tab');
+        let tabName;
+
+        tabNav.forEach(item => {
+            item.addEventListener('click', selectTabNav)
+        });
+
+        function selectTabNav() {
+            tabNav.forEach(item => {
+                item.classList.remove('is-active');
+            });
+            this.classList.add('is-active');
+            tabName = this.getAttribute('data-tab-name');
+            selectTabContent(tabName);
+        }
+
+        function selectTabContent(tabName) {
+            tabContent.forEach(item => {
+                item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+            })
+        }
+    }
+
+    tabToggle();
 })
