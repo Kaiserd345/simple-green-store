ff15c53f9717d0d20f6933f2c5b17974aa4daf71
Dmytro Arkhypov <dimonarh@gmail.com>
2020-11-15 18:53:59 +0200

    refactor js file

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,19 @@
+"use strict"
 $(document).ready(function () {
-    //Sticky Header
-    window.onscroll = () => stickyHeader();
-    const headerTop = document.querySelector(".header");
 
+    window.onscroll = () => stickyHeader();
+    tabToggle();
+    scrollToLink();
+    modalWindow();
+
+    // Functions Declaration
     function stickyHeader() {
-        if (window.pageYOffset >= 150) {
-            headerTop.classList.add("header-sticky");
-        } else {
-            headerTop.classList.remove("header-sticky")
-        }
+        const headerTop = document.querySelector(".header");
+        window.pageYOffset >= 150 ? headerTop.classList.add("header-sticky") : headerTop.classList.remove("header-sticky");
     }
 
-    //Tabs Toggle for Brand Section
-    const tabToggle = function () {
+    //Function Tabs Toggle for Brand Section
+    function tabToggle() {
         const tabNav = document.querySelectorAll('.products-content-categories-button');
         const tabContent = document.querySelectorAll('.cat-tab');
         let tabName;
@@ -37,49 +38,52 @@ $(document).ready(function () {
         }
     }
 
-    tabToggle();
+    //Function Smooth Scroll to Links
+    function scrollToLink() {
+        const anchors = document.querySelectorAll('a[href*="#"]')
 
-    //Smooth Scroll to Links
-    const anchors = document.querySelectorAll('a[href*="#"]')
+        for (let anchor of anchors) {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault()
 
-    for (let anchor of anchors) {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault()
+                const blockID = anchor.getAttribute('href').substr(1)
 
-            const blockID = anchor.getAttribute('href').substr(1)
-
-            document.getElementById(blockID).scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
+                document.getElementById(blockID).scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                })
             })
-        })
-    }
-
-    //Modal window
-    const btnOpenModal = document.querySelector('.delivery-button');
-    const btnCloseModal = document.querySelector('.close-modal');
-    const modalWindow = document.querySelector('.modal');
-    const modalOverlay = document.querySelector('.overlay');
-    const header = document.querySelector('.header');
-
-    const openModal = function () {
-        modalWindow.classList.remove('hidden');
-        modalOverlay.classList.remove('hidden');
-        header.classList.add('hidden');
-    }
-
-    const closeModal = function () {
-        modalWindow.classList.add('hidden');
-        modalOverlay.classList.add('hidden');
-        header.classList.remove('hidden');
-    }
-
-    btnOpenModal.addEventListener('click', openModal);
-    btnCloseModal.addEventListener('click', closeModal);
-    modalOverlay.addEventListener('click', closeModal);
-    document.addEventListener('keydown', function (e) {
-        if (e.key === 'Escape') {
-            if (!modalWindow.classList.contains('hidden')) closeModal();
         }
-    });
+    }
+
+    //Function for Modal window logic
+    function modalWindow() {
+        const btnOpenModal = document.querySelector('.delivery-button');
+        const btnCloseModal = document.querySelector('.close-modal');
+        const modalWindow = document.querySelector('.modal');
+        const modalOverlay = document.querySelector('.overlay');
+        const header = document.querySelector('.header');
+
+        const openModal = function () {
+            modalWindow.classList.remove('hidden');
+            modalOverlay.classList.remove('hidden');
+            header.classList.add('hidden');
+        }
+
+        const closeModal = function () {
+            modalWindow.classList.add('hidden');
+            modalOverlay.classList.add('hidden');
+            header.classList.remove('hidden');
+        }
+
+        btnOpenModal.addEventListener('click', openModal);
+        btnCloseModal.addEventListener('click', closeModal);
+        modalOverlay.addEventListener('click', closeModal);
+        document.addEventListener('keydown', function (e) {
+            if (e.key === 'Escape') {
+                if (!modalWindow.classList.contains('hidden')) closeModal();
+            }
+        });
+
+    }
 })
